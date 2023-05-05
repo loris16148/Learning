a4f1211b8d7e2d1050fb16ca09d875fb345cdc56
loris16148 <loris16148@yahoo.com>
2023-05-05 10:59:08 -0400

    Update MultipleParamatersFunctions.js

--- MultipleParamatersFunctions.js
+++ MultipleParamatersFunctions.js
@@ -1,3 +1,16 @@
+function main() {
+    var eventExample = readLine();
+    // function call
+    setReminder(eventExample)
+}
+//complete the function
+function setReminder(m) {
+   console.log( "You set a reminder about " + m)
+   //console.log("you")
+};
+
+
+
 
 /*Multiple Parameters
 
